7de96163bc3054143b6f53ca7ce2b1baaeca3fb7
Monowar Hossain Khan <mhkha2022@gmail.com>
2023-11-27 02:03:29 +0600

    fix urls.py

--- FamilyTree/urls.py
+++ FamilyTree/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^api/auth/', include('djoser.urls')),
-    path(r'^api/auth/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('Ancestor.urls')),
 ]
