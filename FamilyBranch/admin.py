ac53eda5c66102478e7aac60e6b67b0cd3c4fc32
Monowar Hossain Khan <mhkha2022@gmail.com>
2023-12-15 19:04:29 +0600

    Pipfile update, Admin panel for Person model
    Switched to Arch and updated Pipfile because of downgrade python 3.11

--- FamilyBranch/admin.py
+++ FamilyBranch/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'gender', 'father', 'mother')
+    fieldsets = (
+        (None, {
+            'fields': ['user', 'name', 'gender']
+        }),
+        ('Additional info', {
+            'fields': ('father', 'mother', 'dob', 'date_of_death', 'nationality')
+        }),
+    )
+    search_fields = ('name', 'age', 'gender', 'father', 'mother')
+    autocomplete_fields = ('user', 'father', 'mother')
+    list_filter = ('user', 'gender')
+    empty_value_display = 'N/A'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('father', 'mother')
+
+    def get_search_results(self, request, queryset, search_term):
+        if request.GET.get('field_name') == 'father':
+            return queryset.filter(gender=Person.Gender.MALE).filter(name__icontains=search_term).order_by(
+                'name'), False
+        elif request.GET.get('field_name') == 'mother':
+            return queryset.filter(gender=Person.Gender.FEMALE).filter(name__icontains=search_term).order_by(
+                'name'), False
+        return super().get_search_results(request, queryset, search_term)
+
+
+admin.site.register(Person, PersonAdmin)
